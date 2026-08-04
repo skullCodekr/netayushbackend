5df5a5bf62b68626cd59a263099657e120cd334f
skullCodeKr <yadavvaibhav965@gmail.com>
2026-08-05 04:30:19 +0530

    MAIL TIMEOUT FIX

--- config/emailService.js
+++ config/emailService.js
@@ -6,6 +6,9 @@ const transporter = nodemailer.createTransport({
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
+  connectionTimeout: 30000,
+  greetingTimeout: 30000,
+  socketTimeout: 30000,
 });
 
 const sendOTPEmail = async (toEmail, otp) => {
